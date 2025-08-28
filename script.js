39e9f80a9d0d9cf9fc7127891a28742e521143f6
Csjiago <145134278+Csjiago@users.noreply.github.com>
2025-08-28 11:50:59 -0300

    v ou f ?

--- script.js
+++ script.js
@@ -7,50 +7,38 @@ const textoResultado = document.querySelector(".texto-resultado");
 const  perguntas = [
 {
 
-    enunciado: "Um assistente de voz como a Siri ou a Alexa usa inteligência artificial para entender seus comandos.",
-alternativas:[
-    "Verdadeiro",
-    "Falso"
-  ]
+    enunciado:
+     "Um assistente de voz como a Siri ou a Alexa usa inteligência artificial para entender seus comandos.",
+    alternativas:["Verdadeiro", "Falso"],
+
 },
 {
 
     enunciado: "A IA consegue ter sentimentos e emoções da mesma forma que os humanos.",
-alternativas:[
-     "Verdadeiro",
-     "Falso"
-  ]
+    alternativas: ["Verdadeiro", "Falso"],
+
 },
 {
 
     enunciado: "Carros autônomos, que dirigem sozinhos, são um exemplo de aplicação da IA?",
-alternativas:[
-    "Verdadeiro",
-    "Falso"
-  ]
+alternativas:["Verdadeiro", "Falso"],
+    
+
 },
 {
 
         enunciado: "MACHINE LEARNING é a capacidade de uma máquina aprender sem ser explicitamente programada para cada tarefa. ",
 alternativas:[
-    "Verdadeiro",
-    "Falso"
-  ]
+   alternativas:["Verdadeiro", "Falso"],
 },
 {
 
         enunciado: "Algoritmos de IA são usados apenas em filmes de ficção científica.",
-alternativas:[
-    "Verdadeiro",
-    "Falso"
-  ]
+alternativas:["Verdadeiro", "Falso"],
 },
 {
  
         enunciado: "A IA tem o potencial de automatizar algumas tarefas, o que pode impactar o futuro do trabalho.  ",
-alternativas:[
-    "Verdadeiro",
-    "Falso"
-  ]
+alternativas:["Verdadeiro", "Falso"],
 },
 {    
